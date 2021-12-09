3fd44f13de6066bef61c4ac3dc963741695f34f4
Unicorn5678 <77387530+Unicorn5678@users.noreply.github.com>
2021-12-09 15:45:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+text_input_box = "";
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting objects";
+    text_input_box = document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("Model loaded");
+    status = true;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
